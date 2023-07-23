1db39ac150efa0b2a4cba31a4bad60ddc15371fe
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2023-07-24 00:08:18 +0530

    typography begins...

--- py/edg/ka/edg.py
+++ py/edg/ka/edg.py
@@ -1,36 +1,79 @@
 #!/usr/bin/env python3
-import tkinter as tk
+from tkinter import *
+from typo.z1 import *
 
 
-root=tk.Tk()
-c=tk.Canvas(root,bg='black',width=1280,height=720)
-cmap={
-  'a':[0.0,0.0,0.5,0.0,0.5,1.0,0.0,1.0,0.0,0.5,0.5,0.5],
-  'b':[0.0,0.0,0.0,1.0,0.5,1.0,0.5,0.5,0.0,0.5],
-  'z':[0.0,0.0,1.0,0.0,0.0,1.0,1.0,1.0],
-  'c':[1.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0]
-}
-
-
-# funxionality
-def polyline(cxy,pts,k):
-  if len(pts)<4 or len(pts)%2!=0:
-    raise Exception('illegal polyline invocation!')
-  p0=cxy[0]+k*pts[0],cxy[1]+k*pts[1]
-  i=2
-  while i<len(pts):
-    p1=cxy[0]+k*pts[i],cxy[1]+k*pts[i+1]
-    c.create_line(p0,p1,fill='white')
-    p0=p1
-    i+=2
+class Edg(Tk):
+  def __init__(self):
+    Tk.__init__(self)
+    self.cmap = fntmap
+    self.cursor = 0
+    self.buffer = []
+    self.curloc = [40, 40]
+    self.cgap = 30
+    self.lnht = 40
+    self.ftsz = 36
+    self.wm_title('edg')
+    self.cnv = Canvas(self, bg='black', width=1600, height=900)
+    self.bind_all("<Any-KeyPress>", self.kbpress)
+    self.cnv.pack()
+    
+  def rndrbuf(self):
+    self.cnv.delete('all')
+    dx, dy = self.curloc
+    for c in self.buffer:
+      if c == '\r':
+        dy += self.lnht
+        dx = self.curloc[0]
+      else:
+        self.putch(c, [dx, dy], self.ftsz)
+        dx += self.cgap
+    
+  def kbpress(self, e):
+    kc = e.keycode
+    print(kc)
+    if kc==9:
+      print('sayonara!')
+      raise SystemExit()
+    if kc==66:
+      print('caps lock')
+    elif kc==50:
+      print('shift')
+    elif kc==22:
+      if self.cursor>0:
+        self.cursor -= 1
+        self.buffer.pop(self.cursor)
+        self.rndrbuf()
+      else:
+        print('already at beginning, cannot backspace')
+    elif e.char in '\r qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,./<>?;:\'"[]{}1234567890-=_+|\\':
+      self.buffer.insert(self.cursor, e.char)
+      self.cursor += 1
+      self.rndrbuf()
+    else:
+      print('cannot handle keycode: %d'%kc)
+  
+  def polyline(self, cxy, pts, k):
+    i = 0
+    pxy = []
+    while i < len(pts):
+      pxy.append(cxy[0] + round(pts[i] * k))
+      pxy.append(cxy[1] + round(pts[i+1] * k))
+      i += 2
+    self.cnv.create_line(*pxy, fill='white')
+    
+  def putch(self, ch, cxy, k):
+    if ch == ' ':
+      return
+    if ch not in self.cmap:
+      print('char "%s" not in cmap'%ch)
+      return
+    plns = self.cmap[ch]
+    for pln in plns:
+      self.polyline(cxy, pln, k)
 
 
 # flow begins
-polyline([100,100],cmap['a'],30)
-polyline([200,100],cmap['b'],40)
-polyline([300,100],cmap['z'],50)
-polyline([400,100],cmap['c'],30)
-
-c.pack()
-root.mainloop()
+z = Edg()
+z.mainloop()
 
