e57e74bf0367b05fda9246cf433a6826c2aed4d6
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2023-08-27 20:54:51 +0530

    sense TERM from now

--- py/shc/ga/shc.py
+++ py/shc/ga/shc.py
@@ -1,12 +1,19 @@
 import os
 import u
 import fs
+import sys
 
+termmode='TERM' in os.environ
 pwd=os.getcwd()
+if not termmode:
+  print('[shc] you are not a terminal!', file=sys.stderr)
 
 # flow
 while True:
-  cli=input('shc-> ')
+  if termmode:
+    cli=input('shc-> ')
+  else:
+    cli=input()
   if cli=='q':
     break
   cmd,args=u.cadr(cli)
@@ -17,6 +24,8 @@ while True:
       fs.ls(args,pwd)
     elif cmd=='cat':
       fs.cat(args,pwd)
+    else:
+      print('unknown cmd: ' + cmd, file=sys.stderr)
   except Exception as e:
     print('[ERROR] %s'%str(e))
 
