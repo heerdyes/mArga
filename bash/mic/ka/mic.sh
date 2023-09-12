d598cd31578670923d1ac2ffcd9d50c9f392e2d3
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2023-09-12 23:48:01 +0530

    mic updated for shg

--- bash/mic/ka/mic.sh
+++ bash/mic/ka/mic.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 MARGA_HOME="$HOME/.mArga.d"
 EDG_JAVA_NA="$MARGA_HOME/edg/java/na"
+SHG_JAVA_NA="$MARGA_HOME/shg/java/na"
 MARGA_GH_ROOT="../../.."
 GH_JAVA_EDG_NA="$MARGA_GH_ROOT/java/edg/na"
+GH_JAVA_SHG_NA="$MARGA_GH_ROOT/java/shg/na"
 
 echo "[mArga installer script]"
 if [ -d "$MARGA_HOME" ]; then
@@ -15,32 +17,39 @@ fi
 echo "mArga will now be installed in $MARGA_HOME"
 mkdir -v $MARGA_HOME
 
-echo "[MENU]"
-echo "-> choose programs to be installed from the mArga suite:"
-echo "  1. edg"
-echo "  [NOTE] currently only edg/java/na is released."
-read -p "choice: " ch
-echo "choice -> $ch"
-
-if [ "$ch" == "1" ]; then
-  mkdir -pv $EDG_JAVA_NA
-  pushd $GH_JAVA_EDG_NA
-  ./c
-  if [ "$?" == "0" ]; then
-    echo "edg/java/na compiled successfully!"
-    echo "proceeding to copy classes..."
-    cp -v *.class $EDG_JAVA_NA
-    cp -v edg.cfg $EDG_JAVA_NA
-    echo "creating .mArga dotfile for the funxions..."
-    printf "edg() {\n  java -cp %s edg \$1\n}" $EDG_JAVA_NA > $HOME/.mArga
-    echo "[action] type below command in your shell:"
-    echo ". ~/.mArga"
-  else
-    echo "edg/java/na compilation unsuccessful!"
-    echo "will not install."
-  fi
-  popd
+echo "installing edg/java/na"
+mkdir -pv $EDG_JAVA_NA
+pushd $GH_JAVA_EDG_NA
+./c
+if [ "$?" == "0" ]; then
+  echo "edg/java/na compiled successfully!"
+  echo "proceeding to copy classes..."
+  cp -v *.class $EDG_JAVA_NA
+  cp -v edg.cfg $EDG_JAVA_NA
+  echo "creating .mArga dotfile for the funxions..."
+  printf "edg() {\n  java -cp %s edg \$1\n}\n" $EDG_JAVA_NA > $HOME/.mArga
+  echo "[action] type below command in your shell:"
+  echo ". ~/.mArga"
 else
-  echo "nothing will be installed."
+  echo "edg/java/na compilation unsuccessful!"
+  echo "will not install."
 fi
+popd
 
+echo "installing shg/java/na"
+mkdir -pv $SHG_JAVA_NA
+pushd $GH_JAVA_SHG_NA
+./c
+if [ "$?" == "0" ]; then
+  echo "shg/java/na compiled successfully!"
+  echo "proceeding to copy classes..."
+  cp -v *.class $SHG_JAVA_NA
+  echo "creating .mArga dotfile for the funxions..."
+  printf "shg() {\n  java -cp %s shg\n}\n" $SHG_JAVA_NA >> $HOME/.mArga
+  echo "[action] type below command in your shell:"
+  echo ". ~/.mArga"
+else
+  echo "shg/java/na compilation unsuccessful!"
+  echo "will not install."
+fi
+popd
